30e52bb2c41f3d72f7da8e4417ede8b25c3e7dc6
jomaosssss <joma.ostgo@gmail.com>
2025-04-09 09:00:06 -0600

    Agregar archivos de proyecto.

---
+++ nuevo/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using nuevo.Models;
+
+namespace nuevo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Acción por defecto que muestra la vista Index.cshtml
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Nueva acción que muestra la vista Inicio.cshtml (Dashboard)
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+
+        public IActionResult Agendar()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
