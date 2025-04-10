47d374bf6f66a19ff4b9806ccfb7ff5a0082c2bd
jomaosssss <joma.ostgo@gmail.com>
2025-04-10 12:05:30 -0600

    tablas, iconos, colores, etc

--- nuevo/Program.cs
+++ nuevo/Program.cs
@@ -18,8 +18,9 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Inicio}/{id?}")
     .WithStaticAssets();
+
 
 
 app.Run();
