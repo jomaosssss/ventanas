a00c8f30419fb50aa250539effbd0f5094cadda3
FernandoMendoza93 <angfer2001@gmail.com>
2025-04-14 15:33:00 -0600

    seguimiento de carga de excel

---
+++ nuevo/Views/Home/MCCFEcam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nuevo.Views.Home
+{
+    public class MCCFEcamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
